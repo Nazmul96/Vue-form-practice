8776d092569d7a81eae69cfd914caefc1eda6b26
Nazmul Hossain <hossainnazmul191@gmail.com>
2023-08-06 23:38:28 +0600

    module 6 live test push

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,12 @@
 <script setup>
     import { ref, reactive } from 'vue'
-    const favColors = ref([])
+    const simpletext = ref("Hello")
 </script>
 
 <template>
   <section class="mx-auto container">
-      <h1 class="text-2xl mb-10">Vue Form</h1>
-      <p class="mb-10">{{ favColors }}</p>
-      Your favorite colors <br/>
-      <label for="red">Red</label>
-      <input class="ml-2 mr-2" type="checkbox" id="red" v-model="favColors" value="red">
-      <label for="blue">Blue</label>
-      <input class="ml-2 mr-2" type="checkbox" id="blue" v-model="favColors" value="blue">
-      <label for="yellow">Yellow</label>
-      <input class="ml-2 mr-2" type="checkbox" id="yellow" v-model="favColors" value="yellow">
+    <p class="mb-3">{{ simpletext }}</p>
+     <input type="text" class="p-5" v-model="simpletext"/>
   </section>
 </template>
 
